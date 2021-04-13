7d9ae2c5ef2765e9c3d845a698c4e46e2d7c65ad
Iqbal Ahmed <iqbalhussain9865@gmail.com>
2021-04-14 00:45:48 +0600

    working

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sot4y.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
